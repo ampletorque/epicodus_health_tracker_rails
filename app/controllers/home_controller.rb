e36e6b14401fe7bf24de9e012879360e6be25324
Andrew David Burt & Jim Patt <student@epicodus.com>
2015-07-15 16:53:24 -0700

    final save for the day.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,8 @@ before_filter :authenticate_user!
     @user = current_user
     @foods = Food.all
     @exercises = Exercise.all
+    @eaten_foods = FoodsUser.by_user(current_user)#.since(2.weeks.ago)#.by_food("cheeseburger")
+    # binding.pry
+    @since_foods = FoodsUser.since(2.weeks.ago)
   end
 end
--- app/models/foods_user.rb
+++ app/models/foods_user.rb
@@ -2,4 +2,7 @@ class FoodsUser < ActiveRecord::Base
   validates :quantity, presence: true
   belongs_to :user
   belongs_to :food
+  scope :by_user, lambda { |user| { :conditions => {:user_id => user.id}}}
+  scope :by_food, lambda { |food| { :conditions => {:food_id => food.id}}}
+  scope :since, lambda { |timestamp| {:conditions => {:created_at => (timestamp .. Time.now.utc)}}}
 end
